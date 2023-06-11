cb05e350d97e839b3af5c35eb84fa062b9bd95e8
Zachary Kuchar <zacharyk323@live.com>
2023-06-11 10:36:45 -0400

    Updated chidren content

--- app.js
+++ app.js
@@ -81,33 +81,34 @@ let majorContainer = {
     optionTags: ["Shirts", "Pants", "Shoes", "Coats"],
     productImages: [
       {
-        name: "Sneakers",
-        pic: "https://images.unsplash.com/photo-1527010154944-f2241763d806?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=688&q=80",
+        name: "Shirts",
+        pic: "https://target.scene7.com/is/image/Target/GUEST_26881f52-1230-42c1-afc9-f919da4bdc6c",
       },
       {
         name: "Swimwear",
         pic: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRocde2MIMske_rQLNeNq370sDRUhR2JlLaw&usqp=CAU",
       },
       {
-        name: "Flannels",
-        pic: "https://images.unsplash.com/photo-1638718297700-e828368a54e2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80",
+        name: "Dresses",
+        pic: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMI1yHW9NXOMC3CcLR3xd42YjfkAHM9lUD5g&usqp=CAU",
       },
       {
-        name: "Scarves",
-        pic: "https://images.unsplash.com/photo-1520903920243-00d872a2d1c9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80",
+        name: "Pants",
+        pic: "https://1.bp.blogspot.com/-rgWAHTDoMM4/TVqSuMtYz2I/AAAAAAAAlyk/FlVdTu09Z_M/s1600/a.jpg",
       },
       {
-        name: "Hoodies",
-        pic: "https://images.unsplash.com/photo-1509942774463-acf339cf87d5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80",
+        name: "Sweats",
+        pic: "https://i.etsystatic.com/13694911/r/il/846b9c/3078268171/il_fullxfull.3078268171_hazr.jpg",
       },
       {
-        name: "Hats",
-        pic: "https://images.unsplash.com/photo-1521119989659-a83eee488004?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=723&q=80",
+        name: "Pjs",
+        pic: "https://media.istockphoto.com/id/473707118/photo/weve-got-the-moves-likes-jagger.jpg?s=612x612&w=0&k=20&c=HHsnzHp256n8XDwwjztBYdLz7lyUAEmWLaD2B8rZiQ4=",
       },
     ],
-    textDescription: "Kids dummy text.",
+    textDescription:
+      "Upgrade your little one's wardrobe with a variety of boys' activewear options. We have many of your favorite brands.",
     lowImage:
-      "https://images.unsplash.com/photo-1516826957135-700dedea698c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80",
+      "https://media.istockphoto.com/id/875614000/photo/chilling-before-class-starts.jpg?s=612x612&w=0&k=20&c=FU7NmpZ1K3TeCkcZ7WU98BlZc7m2zN9k0siwif8D2ow=",
   },
 };
 // -----------------------
